80d96904fb5b9eb1548afb850231e4d2a0081ba8
Daniel Todorov <danielntodorov@hotmail.com>
2022-08-11 15:40:23 +0300

    added database migrations to docker

--- go.mod
+++ go.mod
@@ -8,8 +8,15 @@ require (
 )
 
 require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+)
+
+require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/mitchellh/mapstructure v1.3.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ internal/pkg/db/mysql/mysql.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+)
+
+var Db *sql.DB
+
+func InitDB() {
+	db, err := sql.Open("mysql", "root:dbpass@tcp(localhost)/hackernews")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Panic(err)
+	}
+
+	Db = db
+}
+
+func CloseDB() error {
+	return Db.Close()
+}
+
+func Migrate() {
+	if err := Db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	driver, _ := mysql.WithInstance(Db, &mysql.Config{})
+	m, _ := migrate.NewWithDatabaseInstance(
+		"file://internal/pkg/db/migrations/mysql",
+		"mysql",
+		driver,
+	)
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
+}
